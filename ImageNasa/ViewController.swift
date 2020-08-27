7bd76774d827e739050a3c11ac79d292236f1dff
Orlando Sanchez <orlsan@ciencias.unam.mx>
2020-08-27 18:01:51 -0500

    Deselect a clicked cell before opening the image detail

--- ImageNasa/ViewController.swift
+++ ImageNasa/ViewController.swift
@@ -62,6 +62,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
         performSegue(withIdentifier: "detailSegue", sender: self)
     }
 
